4e701df7d19da35034eb1e06141494e259c68310
liapanou7 <liapanou7@gmail.com>
2023-01-09 16:03:29 +0200

    make select and list of images

--- components/EditModal/index.tsx
+++ components/EditModal/index.tsx
@@ -1,6 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+const images = [
+  {
+    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt7XrlOYGCxY4_ESPW5stFkD_pHLunqVVYbQ&usqp=CAU",
+  },
+  {
+    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxXnC3fwMwkbIt3ejGRIw3NmbDyUtgS5g2jA&usqp=CAU",
+  },
+];
 
 export function EditModal(props: { onCancel: () => void; onSave: () => void }) {
+  const [selectedImage, setSelectedImage] = useState();
   return (
     <>
       <div className="modal border ">
@@ -13,7 +23,6 @@ export function EditModal(props: { onCancel: () => void; onSave: () => void }) {
             placeholder="Type here"
             className="input input-bordered w-full"
           />
-
           <label className="label">
             <span className="label-text">Latitude</span>
           </label>
@@ -22,7 +31,6 @@ export function EditModal(props: { onCancel: () => void; onSave: () => void }) {
             placeholder="Type here"
             className="input input-bordered w-full"
           />
-
           <label className="label">
             <span className="label-text">Longtitude</span>
           </label>
@@ -31,7 +39,25 @@ export function EditModal(props: { onCancel: () => void; onSave: () => void }) {
             placeholder="Type here"
             className="input input-bordered w-full"
           />
-
+          <br /> <br />
+          <select className="select select-bordered w-full ">
+            <option disabled selected>
+              Type of game
+            </option>
+            <option>Detect</option>
+            <option>Collect</option>
+          </select>
+          <div className="divider"></div>
+          <div className="grid grid-cols-4 gap-2 ">
+            {images.map((image, idx) => (
+              <img
+                onClick={() => setSelectedImage(image.src)}
+                key={idx}
+                src={image.src}
+              ></img>
+            ))}
+          </div>
+          <div className="divider"></div>
           <div className="modal-action">
             <button onClick={props.onCancel} className="btn btn-sm">
               Cancel
